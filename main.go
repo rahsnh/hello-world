00a41ac5c3e8e8614fbc4aa56b44a5d16ccd9762
Rahul Sinha <rahul@Rahuls-MacBook-Air.local>
2025-10-13 21:54:43 +0530

    updated main file

--- main.go
+++ main.go
@@ -12,6 +12,6 @@ func handler(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	http.HandleFunc("/", handler)
-	fmt.Println("Server starting on port 8080...")
+	fmt.Println("Server started on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
